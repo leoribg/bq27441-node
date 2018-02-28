407dc3b6be2097c1e2fe4a1a4e6b113759a9a531
Leonardo <leonardo.ribeiro@gee.inatel.br>
2018-02-28 21:55:49 +0000

    add comments

--- app.js
+++ app.js
@@ -2,28 +2,22 @@ var i2c = require('i2c');
 var address = 0x55;
 var wire = new i2c(address, {device: '/dev/i2c-2'}); // point to your i2c address, debug provides REPL interface
 
+/* Enable chip*/
 wire.writeBytes(0x00, [0x00, 0x80], function(err) {});
 
-wire.on('data', function(data) {
-  console.log(data);
-  // result for continuous stream contains data buffer, address, length, timestamp 
-});
-
-wire.scan(function(err, data) {
-  console.log(data);
-  // result contains an array of addresses 
-});
-
+/* Read voltage register 0x04 | 0x80 */
 wire.readBytes(0x84, 2, function(err, res) {
   console.log(res);
   // result contains a buffer of bytes 
 });
 
+/* Read temperature register 0x02 | 0x80 */
 wire.readBytes(0x82, 2, function(err, res) {
   console.log(res);
   // result contains a buffer of bytes 
 });
 
+/* Read current register 0x10 | 0x80 */
 wire.readBytes(0x90, 2, function(err, res) {
   console.log(res);
   // result contains a buffer of bytes 
