07d4e3692831cb698e85e280a84fba2b5db3f29b
Leonardo <leonardo.ribeiro@gee.inatel.br>
2018-03-01 17:57:47 +0000

    Calculate and write checksum

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var i2c = require('i2c');
 var address = 0x55;
 var wire = new i2c(address, {device: '/dev/i2c-2'}); // point to your i2c address, debug provides REPL interface
+var total =  0;
 
 /* Unseal chip*/
 wire.writeBytes(0x00, [0x00, 0x80], function(err) {});
@@ -20,6 +21,22 @@ wire.writeBytes(0x3F, [0x00], function(err) {});
 
 /* Set capacity 0x04B0 = 1200mAh */
 wire.writeBytes(0x4A, [0x04, 0xB0], function(err) {});
+
+/* Calculate new checksum*/
+wire.readBytes(0xC0, 32, function(err, res) {  
+
+  for(var i = 0; i < res.length; i++)
+  {                  
+    total += Number(res[i]);
+  }
+
+  total = total % 256;
+  console.log(total.toString(16));
+  // result contains a buffer of bytes 
+});
+
+/* Write new checksum. */
+wire.writeBytes(0x60, [total], function(err) {});
 
 /* Exit CFGUPDATE mode by sending SOFT_RESET subcommand */
 wire.writeBytes(0x00, [0x42, 0x00], function(err) {});
@@ -47,6 +64,12 @@ wire.readBytes(0x90, 2, function(err, res) {
 
 /* Read device capacity register 0x4A | 0x80 */
 wire.readBytes(0xCA, 2, function(err, res) {
+  console.log(res);
+  // result contains a buffer of bytes 
+});
+
+/* Read device capacity register 0x0C | 0x80 */
+wire.readBytes(0x8C, 2, function(err, res) {
   console.log(res);
   // result contains a buffer of bytes 
 });
