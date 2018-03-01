4edac76df04929ed207fb3d99626992dfc247343
Leonardo <leonardo.ribeiro@gee.inatel.br>
2018-03-01 12:23:47 +0000

    Include device capacity command

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var wire = new i2c(address, {device: '/dev/i2c-2'}); // point to your i2c addres
 
 /* Enable chip*/
 wire.writeBytes(0x00, [0x00, 0x80], function(err) {});
+wire.writeBytes(0x00, [0x00, 0x80], function(err) {});
+
+/* Set capacity 0x04B0 = 1200mAh */
+wire.writeBytes(0x4A, [0x04, 0xB0], function(err) {});
 
 /* Read voltage register 0x04 | 0x80 */
 wire.readBytes(0x84, 2, function(err, res) {
@@ -19,6 +23,12 @@ wire.readBytes(0x82, 2, function(err, res) {
 
 /* Read current register 0x10 | 0x80 */
 wire.readBytes(0x90, 2, function(err, res) {
+  console.log(res);
+  // result contains a buffer of bytes 
+});
+
+/* Read device capacity register 0x4A | 0x80 */
+wire.readBytes(0xCA, 2, function(err, res) {
   console.log(res);
   // result contains a buffer of bytes 
 });
